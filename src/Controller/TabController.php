ff91b9d70ba009aa2ab6a3764947d21f7d932b98
asma-ab <abichouasmaa@gmail.com>
2024-03-14 01:29:34 +0100

    heritage in twig

--- src/Controller/TabController.php
+++ src/Controller/TabController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +17,20 @@ class TabController extends  AbstractController
         }
         return $this->render('tab/tab.html.twig', [
             'notes' => $notes,
+        ]);
+    }
+    #[Route('tab/users', name: 'tab.users')]
+    public function users(): Response
+    {
+        $users = [
+            ['firstName'=> 'asma' , 'lastName'=>'Abichou' , 'age'=> 24],
+            ['firstName'=> 'imen' , 'lastName'=>'Abichou' , 'age'=> 34],
+            ['firstName'=> 'olfa' , 'lastName'=>'Abichou' , 'age'=> 36],
+            ['firstName'=> 'amira' ,'lastName'=>'Abichou' , 'age'=> 35],
+
+        ];
+        return $this->render('tab/users.html.twig', [
+            'users' => $users
         ]);
     }
 }
