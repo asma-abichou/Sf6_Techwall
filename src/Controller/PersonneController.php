83d9c5f1f58dd38064da2910b4e19983efa939de
asma-ab <abichouasmaa@gmail.com>
2024-03-15 23:15:33 +0100

    add person

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
+use PhpParser\ErrorHandler\Collecting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PersonneController extends AbstractController
 {
-    #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    #[Route('/personne/add', name: 'personne')]
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
+       // $this->getDoctrine(); symfony <=5
+        $entityManger = $doctrine->getManager();
+        $personne = new Personne();
+        $personne->setFirstName('Asma');
+        $personne->setLastName('Abichou');
+        $personne->setAge('25');
+
+        // insert personne
+        $entityManger->persist($personne);
+        $entityManger->flush();
+        //execute transaction
+        return $this->render('personne/details.html.twig', [
+            'personne' => $personne,
         ]);
     }
 }
