bff2ce08a81ad54cbb7a6646954a4d83fa24d2d1
asma-ab <abichouasmaa@gmail.com>
2024-03-16 02:04:03 +0100

    automatic paging functionality

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -20,6 +20,32 @@ class PersonneController extends AbstractController
             'personnes'=> $personnes
         ]);
     }
+    #[Route('/all/{page?1}/{nbre?12}', name: 'personne.list.all')]
+    public function indexall(ManagerRegistry $doctrine, $page, $nbre):Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $nbPersonne = $repository->count([]);
+        $nbrePage = ceil($nbPersonne / $nbre);
+        //dd(ceil($nbPersonne / $nbre));
+        $personnes = $repository->findBy([],[], $nbre, ($page - 1) * $nbre);
+        return $this->render('personne/index.html.twig' , [
+            'personnes'=> $personnes,
+            'isPaginated'=> true,
+            'nbrePage'=>$nbrePage,
+            'page' => $page,
+            'nbre'=>$nbre
+        ]);
+    }
+    #[Route('/{id<\d+>}', name: 'personne.detail')]
+    public function detail(Personne $personne = null):Response
+    {
+        if(!$personne) {
+            $this->addFlash('error', "La personne n'existe pas");
+            return $this->redirectToRoute('personne.list');
+        }
+
+        return $this->render('personne/details.html.twig', ['personne' => $personne]);
+    }
     #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
