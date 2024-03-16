a67f7678a9919e15bd54310dcdd40050edfbee91
asma-ab <abichouasmaa@gmail.com>
2024-03-16 02:46:04 +0100

    add route to delete person

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -6,50 +6,54 @@ use App\Entity\Personne;
 use Doctrine\Persistence\ManagerRegistry;
 use PhpParser\ErrorHandler\Collecting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 #[Route('/personne')]
 class PersonneController extends AbstractController
 {
     #[Route('/', name: 'personne.list')]
-    public function index(ManagerRegistry $doctrine):Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $repository = $doctrine->getRepository(Personne::class);
         $personnes = $repository->findAll();
-        return $this->render('personne/index.html.twig' , [
-            'personnes'=> $personnes
+        return $this->render('personne/index.html.twig', [
+            'personnes' => $personnes
         ]);
     }
+
     #[Route('/all/{page?1}/{nbre?12}', name: 'personne.list.all')]
-    public function indexall(ManagerRegistry $doctrine, $page, $nbre):Response
+    public function indexall(ManagerRegistry $doctrine, $page, $nbre): Response
     {
         $repository = $doctrine->getRepository(Personne::class);
         $nbPersonne = $repository->count([]);
         $nbrePage = ceil($nbPersonne / $nbre);
         //dd(ceil($nbPersonne / $nbre));
-        $personnes = $repository->findBy([],[], $nbre, ($page - 1) * $nbre);
-        return $this->render('personne/index.html.twig' , [
-            'personnes'=> $personnes,
-            'isPaginated'=> true,
-            'nbrePage'=>$nbrePage,
+        $personnes = $repository->findBy([], [], $nbre, ($page - 1) * $nbre);
+        return $this->render('personne/index.html.twig', [
+            'personnes' => $personnes,
+            'isPaginated' => true,
+            'nbrePage' => $nbrePage,
             'page' => $page,
-            'nbre'=>$nbre
+            'nbre' => $nbre
         ]);
     }
+
     #[Route('/{id<\d+>}', name: 'personne.detail')]
-    public function detail(Personne $personne = null):Response
+    public function detail(Personne $personne = null): Response
     {
-        if(!$personne) {
+        if (!$personne) {
             $this->addFlash('error', "La personne n'existe pas");
             return $this->redirectToRoute('personne.list');
         }
 
         return $this->render('personne/details.html.twig', ['personne' => $personne]);
     }
+
     #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
-       // $this->getDoctrine(); symfony <=5
+        // $this->getDoctrine(); symfony <=5
         $entityManger = $doctrine->getManager();
         $personne = new Personne();
         $personne->setFirstName('Asma');
@@ -63,5 +67,22 @@ class PersonneController extends AbstractController
         return $this->render('personne/details.html.twig', [
             'personne' => $personne,
         ]);
+    }
+
+    #[Route('/delete/{id}', name: 'personne.delete')]
+    public function delete(Personne $personne = null, ManagerRegistry $doctrine): RedirectResponse
+    {
+       //get person to delete
+        // if exist delete and return flash success massage
+            if($personne){
+                $manager = $doctrine->getManager();
+                $manager->remove($personne);
+                $manager->flush();
+                $this->addFlash('success',"la personne a été supprimer avec succés");
+            }else{
+                //if not return flash error massage
+                $this->addFlash('error',"la personne n'existe pas");
+            }
+           return $this->redirectToRoute('personne.list.all');
     }
 }
